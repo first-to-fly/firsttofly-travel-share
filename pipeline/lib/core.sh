84b010d42dc5794fe8a860af73b5f80b79382e8b
Khoa Nguyen <khoa.nguyen@notabasement.com>
2019-05-22 19:27:09 +0700

    Add checkNodeVersion

--- pipeline/lib/core.sh
+++ pipeline/lib/core.sh
@@ -203,3 +203,19 @@ function finally() {
 
   exit "${CATCHED_EXIT_CODE:-}"
 }
+
+# Node
+function checkNodeVersion() {
+
+  if [[ ! -f "./.nvmrc" ]]; then
+    return
+  fi
+
+  NODE_VERSION="v$(head -n 1 "./.nvmrc")"
+  CURRENT_NODE_VERSION="$(node -v)"
+
+  if [[ "${NODE_VERSION}" != "${CURRENT_NODE_VERSION}" ]]; then
+    echo "Expect NodeJS version ${NODE_VERSION} but have ${CURRENT_NODE_VERSION}." >&2
+    exit 1
+  fi
+}
