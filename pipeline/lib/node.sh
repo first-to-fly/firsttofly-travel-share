57160f71cd36ee0e39b83a6a447ace2e94fc4670
Khoa Nguyen <khoa.nguyen@notabasement.com>
2019-06-04 15:31:29 +0700

    Fix node version in scripts

--- pipeline/lib/node.sh
+++ pipeline/lib/node.sh
@@ -2,17 +2,52 @@
 
 function checkNodeVersion() {
 
-  NODE_VERSION="$(jq --raw-output ".devDependencies.node" "./package.json")"
+  EXPECTED_NODE_VERSION="$(head -n 1 ./.nvmrc)"
   CURRENT_NODE_VERSION="$(node -v)"
 
-  if [[ "v${NODE_VERSION}" != "${CURRENT_NODE_VERSION}" ]]; then
-    echo "Expect NodeJS version ${NODE_VERSION} but have ${CURRENT_NODE_VERSION}." >&2
+  if [[ "${CURRENT_NODE_VERSION}" != "v${EXPECTED_NODE_VERSION}" ]]; then
+
+    echo "Expect NodeJS version ${EXPECTED_NODE_VERSION} but have ${CURRENT_NODE_VERSION}." >&2
+
+    # Check and install FNM if needed
+    if ! command -v "fnm" >/dev/null; then
+      if command -v "brew" >/dev/null; then
+        (
+          set -x
+          brew install "Schniz/tap/fnm"
+        )
+      else
+        (
+          set -x
+          curl -fsSL "https://github.com/Schniz/fnm/raw/master/.ci/install.sh" | bash
+        )
+      fi
+    fi
+
+    # Check if expected version is already installed
+    if ! fnm ls | grep "${EXPECTED_NODE_VERSION}" >/dev/null; then
+      (
+        set -x
+        fnm install "${EXPECTED_NODE_VERSION}"
+      )
+    fi
+
+    (
+      set -x
+      fnm use "${EXPECTED_NODE_VERSION}"
+    )
+
+    eval "$(fnm env --multi)"
+
+    CURRENT_NODE_VERSION="$(node -v)"
+
+  fi
+
+  echo
+
+  if [[ "${CURRENT_NODE_VERSION}" != "v${EXPECTED_NODE_VERSION}" ]]; then
     exit 1
   fi
 }
-
-if [[ -d "./node_modules" ]]; then
-  PATH="${PWD}/node_modules/.bin:${PATH}"
-fi
 
 checkNodeVersion
