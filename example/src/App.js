cfe912fd0b152ccfde306fc7b541db6e2fbe60a1
Khoa Nguyen <khoa.nguyen@notabasement.com>
2019-09-19 02:09:42 +0700

    Add example

---
+++ example/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log("");
+  }
+
+  render() {
+
+    return (
+      <div className="App" />
+    );
+  }
+
+}
+
+
+export default App;
