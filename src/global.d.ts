e53e6828e437557192db43cd15b77aaab7212c93
Khoa Nguyen <khoa.nguyen@notabasement.com>
2019-09-30 13:00:16 +0700

    Add some instructions for global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,9 @@
+// Follow https://stackoverflow.com/a/51114250
+// - No "import ...", else this file is treated as a module and no longer global
+// - Use "import()" if needed for types
+// - Use "declare namespace" (add) instead of "declare module" (overwrite)
+
+
 declare namespace NodeJS {
 
   interface Process {
