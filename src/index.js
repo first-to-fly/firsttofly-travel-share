e62f61a49a0dcdd9b60327de0984aca616551fcf
Khoa Nguyen <khoa.nguyen@notabasement.com>
2019-12-01 09:24:42 +0700

    Fix "Cannot use import statement outside a module" for JS files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import sample from "@nabstudio/sample";
-
 import sampleTS from "sampleTS";
 
 import sampleJS from "./sampleJS";
+
+
+(async () => {
+  console.log("Dynamically loading module...");
+  const dynamicSample = await import("./sampleJS");
+  console.log("Module loaded", dynamicSample);
+})();
 
 
 export {
