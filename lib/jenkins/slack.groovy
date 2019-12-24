ebd03bf9b3461e161196c29c16621e7bd5d55823
Khoa Nguyen <khoa.nguyen@notabasement.com>
2019-12-25 01:41:19 +0700

    Try message

--- lib/jenkins/slack.groovy
+++ lib/jenkins/slack.groovy
@@ -8,7 +8,7 @@ String currentRunDescription() {
 
 void send(Map args) { // String channel, String message, String<good|normal|warning|danger> color, Map fields, Map actions, boolean excludeParams
 
-  def text = "${args.message.replace('#BUILD', currentRunDescription())} [<${BUILD_URL}/console|Console>|<${RUN_DISPLAY_URL}|BlueOcean>]\nCommit by *${GIT_COMMITTER_NAME}* <${GIT_COMMITTER_EMAIL}>"
+  def text = "${args.message.replace('#BUILD', currentRunDescription())} [<${BUILD_URL}/console|Console>|<${RUN_DISPLAY_URL}|BlueOcean>]\nCommit by *${GIT_COMMITTER_NAME}* - ${GIT_COMMITTER_EMAIL}"
   args.actions.each { String key, String value ->
     text = "${text} [<${value}|${key}>]"
   }
