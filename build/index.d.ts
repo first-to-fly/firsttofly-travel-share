42315d5e5cf52f2dabe89a051d2024ea288803b4
khoa.nguyen <khoa.nguyen@nabstudio.com>
2021-05-14 02:04:27 +0700

    Build lib

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,4 @@
-import sample from "./@nabstudio/sample";
+import sample from "@nabstudio/sample";
+import sampleTS from "sampleTS";
 import sampleJS from "./sampleJS";
-import sampleTS from "./sampleTS";
-export { sample, sampleJS, sampleTS };
+export { sample, sampleJS, sampleTS, };
--- build/sampleTS.js
+++ build/sampleTS.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.sample = void 0;
 const sample_1 = __importDefault(require("@nabstudio/sample"));
 exports.sample = sample_1.default;
 const someArray2 = [
