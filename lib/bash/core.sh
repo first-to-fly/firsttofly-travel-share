ff40fb8ced5c0c3776a7ef4ae2b9dfde5a257707
Khoa Nguyen <khoa@inkr.com>
2021-09-07 00:40:59 +0700

    Remove "sudo yum install jq"

--- lib/bash/core.sh
+++ lib/bash/core.sh
@@ -208,12 +208,6 @@ function dependency() {
           brew install "jq"
         )
         echo
-      elif command -v "yum" >/dev/null; then
-        (
-          set -x
-          sudo yum install -y "jq"
-        )
-        echo
       else
         (
           set -x
