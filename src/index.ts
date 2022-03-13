f9c2c3d29bcebf941c46b2ac2051f35a838b2c82
Khoa Nguyen <khoa@inkr.com>
2022-03-13 13:09:21 +0700

    Update sample

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import sampleTS from "sampleTS";
-
-import sampleJS from "./sampleJS";
-
-
-(async () => {
-  console.log("Dynamically loading module...");
-  const dynamicSample = await import("./sampleJS");
-  console.log("Module loaded", dynamicSample);
-})();
-
-console.log("sampleJS", sampleJS);
-console.log("sampleTS", sampleTS);
+import "./sample";
