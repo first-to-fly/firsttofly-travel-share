ca70b221958ff0974b75ac8c6bd39516f497428c
Khoa Nguyen <khoa@inkr.com>
2023-11-20 16:19:30 +0700

    Remove deprecated rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,10 +7,7 @@ module.exports = {
 
   rules: {
     "color-function-notation": "legacy",
-    indentation: [2, { ignore: "value" }],
-    "max-line-length": 200,
     "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["global"] }],
-    "string-quotes": "double",
     "scss/at-extend-no-missing-placeholder": null, // Allows @extend .class
     "scss/no-global-function-names": null, // math and color.adjust cannot compile
   },
