22275fc1d74084c52f244f46d314dc7a824d0485
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-03-21 16:44:10 +0700

    declare event

--- src/entities/Settings/Product/Sector.ts
+++ src/entities/Settings/Product/Sector.ts
@@ -4,6 +4,12 @@ import { EntityZ } from "../../entity";
 import { EntityType } from "../../entityType";
 
 
+export enum SectorEvents {
+  SECTOR_UPDATED = "SECTOR_UPDATED",
+  SECTOR_LIST_UPDATED = "SECTOR_LIST_UPDATED",
+}
+
+
 export const SectorZ = EntityZ.extend({
   entityType: z.literal(EntityType.SECTOR),
 
