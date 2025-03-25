19ccdf073ff6a25f9a5bceafe22cd2fcad2cdbd7
Cong Hoang <hoang.cong@notabasement.com>
2025-03-25 11:45:01 +0700

    clean up code

--- src/authorization/ts-authorization/auth-redis-cache-key.ts
+++
@@ -1,7 +0,0 @@
-export function genEntityAuthRedisCacheKey(oid: string): string {
-  return `auth:entity:${oid}`;
-}
-
-export function genCollaborationAuthRedisCacheKey(userId: string, oid: string): string {
-  return `auth:collaboration:${userId}:${oid}`;
-}
