0cda4ab8edb40b67a96c1ed8a2cfc4d212c10469
keninkr <97826621+keninkr@users.noreply.github.com>
2025-03-29 00:13:56 +0700

    update contract

--- src/api/settings/product/contract.ts
+++ src/api/settings/product/contract.ts
@@ -2,18 +2,20 @@ import { initContract } from "@ts-rest/core";
 
 import { badgeContract } from "./badge-contract";
 import { costingItemContract } from "./costing-item-contract";
+import { costingTemplateContract } from "./costing-template-contract";
+import { insuranceDiscountContract } from "./insurance-discount-contract";
 import { mealContract } from "./meal-contract";
 import { productTypeContract } from "./product-type-contract";
 import { roomConfigContract } from "./room-config-contract";
 import { sectorContract } from "./sector-contract";
 import { sectorGroupContract } from "./sector-group-contract";
 import { tagContract } from "./tag-contract";
-import { insuranceDiscountContract } from "./insurance-discount-contract";
 
 
 export const productSettingContract = initContract().router({
   badge: badgeContract,
   costingItem: costingItemContract,
+  costingTemplate: costingTemplateContract,
   meal: mealContract,
   productType: productTypeContract,
   roomConfig: roomConfigContract,
