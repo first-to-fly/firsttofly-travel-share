281e70635e1083c2bbeb578fae3330450090594c
Cong Hoang <hoang.cong@notabasement.com>
2025-04-15 17:40:53 +0700

    simplify referenceCode

--- src/api/settings/general/reference-code-contract.ts
+++ src/api/settings/general/reference-code-contract.ts
@@ -1,12 +1,12 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
 
-import { ReferenceCodeTemplateZ } from "../../../entities/Settings/General/ReferenceCodeTemplate";
+import { ReferenceCodeZ } from "../../../entities/Settings/General/ReferenceCode";
 
 
 const basePath = "/api/settings/reference-code-templates";
 
-const CreateReferenceCodeTemplateZ = ReferenceCodeTemplateZ.pick({
+const CreateReferenceCodeZ = ReferenceCodeZ.pick({
   tenantOID: true,
   name: true,
   moduleId: true,
@@ -18,15 +18,15 @@ const CreateReferenceCodeTemplateZ = ReferenceCodeTemplateZ.pick({
   componentIds: true,
 });
 
-const UpdateReferenceCodeTemplateZ = CreateReferenceCodeTemplateZ.omit({
+const UpdateReferenceCodeZ = CreateReferenceCodeZ.omit({
   tenantOID: true,
 }).partial();
 
-export type UpdateReferenceCodeTemplate = z.infer<typeof UpdateReferenceCodeTemplateZ>;
-export type CreateReferenceCodeTemplate = z.infer<typeof CreateReferenceCodeTemplateZ>;
+export type UpdateReferenceCodeTemplate = z.infer<typeof UpdateReferenceCodeZ>;
+export type CreateReferenceCodeTemplate = z.infer<typeof CreateReferenceCodeZ>;
 
-export const referenceCodeTemplateContract = initContract().router({
-  getReferenceCodeTemplates: {
+export const referenceCodeContract = initContract().router({
+  getReferenceCodes: {
     summary: "Get reference code templates",
     method: "GET",
     path: basePath,
@@ -40,55 +40,55 @@ export const referenceCodeTemplateContract = initContract().router({
     },
   },
 
-  createReferenceCodeTemplate: {
-    summary: "Create a new reference code template",
-    method: "POST",
-    path: basePath,
-    body: CreateReferenceCodeTemplateZ,
-    responses: {
-      200: z.string(),
-    },
-  },
+  // createReferenceCodeTemplate: {
+  //   summary: "Create a new reference code template",
+  //   method: "POST",
+  //   path: basePath,
+  //   body: CreateReferenceCodeZ,
+  //   responses: {
+  //     200: z.string(),
+  //   },
+  // },
 
-  updateReferenceCodeTemplate: {
+  updateReferenceCode: {
     summary: "Update an existing reference code template",
     method: "PATCH",
-    path: `${basePath}/:referenceCodeTemplateOID`,
-    body: UpdateReferenceCodeTemplateZ,
+    path: `${basePath}/:referenceCodeOID`,
+    body: UpdateReferenceCodeZ,
     responses: {
       200: z.string(),
     },
   },
 
-  updateReferenceCodeTemplates: {
+  updateReferenceCodes: {
     summary: "Update multiple existing reference code templates",
     method: "POST",
     path: `${basePath}/batch-update`,
     body: z.record(
       z.string().describe("OID of reference code template to update"),
-      UpdateReferenceCodeTemplateZ,
+      UpdateReferenceCodeZ,
     ),
     responses: {
       200: z.array(z.string().describe("OIDs of updated reference code templates")),
     },
   },
 
-  deleteReferenceCodeTemplate: {
+  deleteReferenceCode: {
     summary: "Delete a reference code template",
     method: "DELETE",
-    path: `${basePath}/:referenceCodeTemplateOID`,
+    path: `${basePath}/:referenceCodeOID`,
     body: z.object({}),
     responses: {
       200: z.boolean(),
     },
   },
 
-  deleteReferenceCodeTemplates: {
+  deleteReferenceCodes: {
     summary: "Delete multiple reference code templates",
     method: "POST",
     path: `${basePath}/batch-delete`,
     body: z.object({
-      referenceCodeTemplateOIDs: z.array(z.string().describe("OIDs of reference code templates to delete")),
+      referenceCodeOIDs: z.array(z.string().describe("OIDs of reference code templates to delete")),
     }),
     responses: {
       200: z.boolean(),
