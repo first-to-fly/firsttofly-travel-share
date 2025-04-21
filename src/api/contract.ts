6dcccab65a88e0d47121f7530481f0f753853afa
Cong Hoang <hoang.cong@notabasement.com>
2025-04-21 13:24:46 +0700

    add contract for IATA

--- src/api/contract.ts
+++ src/api/contract.ts
@@ -1,14 +1,18 @@
 import { initContract } from "@ts-rest/core";
 
+import { activityContract } from "./activity/activity.contract";
 import { authorizationContract } from "./authorization/contract";
 import { documentContract } from "./document/document-contract";
 import { i18nContract } from "./i18n/contract";
+import { iataContract } from "./iata/iata.contract";
 import { productsContract } from "./products/contract";
 import { settingsContract } from "./settings/contract";
 import { termContract } from "./term/term-contract";
 
 
 export const apiContract = initContract().router({
+  activity: activityContract,
+  iata: iataContract,
   i18n: i18nContract,
   products: productsContract,
   settings: settingsContract,
