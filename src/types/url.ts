9a6f7a6580163d46863d05495c362104dddd3810
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-05-29 16:05:30 +0700

    enhance namedurl

--- src/types/url.ts
+++ src/types/url.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 export const NamedURLZ = z.object({
   name: z.string(),
   url: z.string(),
+  mimeType: z.string().optional(),
+  size: z.number().optional(),
 });
 
 export type NamedURL = z.infer<typeof NamedURLZ>;
