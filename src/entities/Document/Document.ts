1adf8f916e92353e8af123bba3d90f55202ffa1a
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-06-09 16:53:05 +0700

    Introduce DocumentType enum and update DocumentZ schema to use it for type validation

--- src/entities/Document/Document.ts
+++ src/entities/Document/Document.ts
@@ -11,12 +11,25 @@ export enum DocumentEvents {
   DOCUMENT_LIST_UPDATED = "DOCUMENT_LIST_UPDATED",
 }
 
+export enum DocumentType {
+  PASSPORT = "passport",
+  NATIONAL_ID = "national_id",
+  DRIVING_LICENSE = "driving_license",
+  VISA = "visa",
+  MARRIAGE_CERTIFICATE = "marriage_certificate",
+  BIRTH_CERTIFICATE = "birth_certificate",
+  TRAVEL_PERMIT = "travel_permit",
+  OTHER = "other",
+}
+
+export const DocumentTypeZ = z.nativeEnum(DocumentType);
+
 export const DocumentZ = EntityZ.extend({
   entityType: z.literal(EntityType.DOCUMENT),
 
   entityOID: z.string(), // linkage to the entity that owns this document
 
-  type: z.string(),
+  type: DocumentTypeZ,
   name: z.string(),
   docIdentification: z.string(),
 
